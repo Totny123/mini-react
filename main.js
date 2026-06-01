323534fdda0c75d473294e24e3344decea7540ce
totny <totny97@gmail.com>
2026-06-01 15:30:45 +0800

    将入口文件和根组件进行拆分

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-import React from './core/React.js';
 import ReactDOM from './core/ReactDOM.js';
+import App from './app.js';
 
-const rootContainer = document.querySelector('#root');
-
-const divEl = React.createElement('div', { id: 'app' }, 'hello', ' mini-react');
-
-ReactDOM.createRoot(rootContainer).render(divEl);
+ReactDOM.createRoot(document.querySelector('#root')).render(App);
